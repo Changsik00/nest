d9415eea0a6170f5fa879dfc521498b357c01f3d
Changsik00 <lowmans00@gmail.com>
2020-11-17 23:49:55 +0900

    [add] create orm, add user module

---
+++ src/users/users.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Post, Body, Get, Put, Delete, Param } from '@nestjs/common'
+import { UsersService } from './users.service'
+import { User } from './user.entity'
+
+@Controller('users')
+export class UsersController {
+  constructor(private service: UsersService) {}
+
+  @Get()
+  getAll() {
+    return this.service.findAll()
+  }
+
+  @Get(':id')
+  get(@Param() params) {
+    return this.service.findOne(params.id)
+  }
+
+  @Post()
+  create(@Body() user: User) {
+    console.log('#@# user constroller create', user)
+    return this.service.create(user)
+  }
+
+  @Put(':id')
+  update(@Param() params, @Body() user: User) {
+    return this.service.update(params.id, user)
+  }
+
+  @Delete(':id')
+  delete(@Param() params) {
+    return this.service.delete(params.id)
+  }
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { User } from './user.entity'
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return await this.usersRepository.find()
+  }
+
+  async findOne(id: string): Promise<User> {
+    return await this.usersRepository.findOne(id)
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.usersRepository.delete(id)
+  }
+
+  async create(user: User) {
+    await this.usersRepository.save(user)
+  }
+
+  async update(id: number, updatedUser: User): Promise<User | null> {
+    const user = await this.usersRepository.findOneOrFail(id)
+    if (!user.id) {
+      console.error("User doesn't exist")
+    }
+    await this.usersRepository.update(id, updatedUser)
+    return await this.usersRepository.findOne(id)
+  }
+}
